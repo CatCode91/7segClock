1d795110dca0f9b382186baa4dac930b02f86575
CatCode91 <stupakevich.daniel@gmail.com>
2020-02-01 01:21:27 +0300

    added Time lybrary

--- sdvig_reg/sdvig_reg.ino
+++ sdvig_reg/sdvig_reg.ino
@@ -1,10 +1,13 @@
-#include <Wire.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include "Wire.h"
+#include "OneWire.h"
+#include "DallasTemperature.h"
+#include "TimerOne.h"
+#include "DS3232RTC.h"
 
 // Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 7
- 
+#define ONE_WIRE_BUS 13
+DS3232RTC myRTC;
+
 // Setup a oneWire instance to communicate with any OneWire devices 
 // (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -12,26 +15,24 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-int counter = 7;
+const byte Sseg_pins[3] = {16,15,14}; 
+const byte Matrix_pins[3] = {2,3,4}; 
 
-const byte Sseg_pins[3] = {8,9,10}; 
-const byte Matrix_pins[3] = {17,16,15}; 
+const byte Sdata_pin = 10;
+const byte Ssh_pin = 11;
+const byte Sst_pin = 12;
 
-const byte Sdata_pin = 3;
-const byte Ssh_pin = 4;
-const byte Sst_pin = 5;
+const byte Mdata_pin = 7;
+const byte Msh_pin = 5;
+const byte Mst_pin = 6;
 
-//const byte btn_1 = 13;
-const byte btn_2 = 19;
-
-const byte Mdata_pin = 0;
-const byte Msh_pin = 1;
-const byte Mst_pin = 2;
-
-byte i, second, minute, hour, date, month, year;
+//const byte btn_1 = 3;
+//const byte btn_2 = 4;
 
 //Temp C - D6, Time - SCL - D11, SCA - D12
+//byte i, second, minute, hour, date, month, year;
 
+//числа 1-0
 const byte digits[12] = {
     B11101110, 
     B10000010, 
@@ -47,6 +48,7 @@ const byte digits[12] = {
     B00010000
 };
 
+//число 2 на матрице
 byte Matrix_2[3]= {
   B0110000,
   B0000110, 
@@ -54,18 +56,22 @@ byte Matrix_2[3]= {
  
 };
 
+//число 1 на матрице
 byte Matrix_0[3]= {
   B1111111, 
   B1111111,
   B1111111,
 };
 
+//число 1 на матрице
 byte Matrix_1[3]= {
   B0000000, 
   B1011111,
   B1101111,
 };
 
+
+//буква t на матрице
 byte Matrix_t[3]= {
   B1110110, 
   B1100000,
@@ -73,24 +79,33 @@ byte Matrix_t[3]= {
 };
 
 
-void ShowTemperature()
+void ShowTemperature(int16_t rtcTemp)
 {
-  float t = sensors.getTempCByIndex(0);
-      
-  float t_1 = t/10;
-  //float t_2 = t%10;
+  char outstr[15];
+  dtostrf(rtcTemp,2,1, outstr);
   
-  float temp;
+  //float c = rtcTemp / 4.;
+  int t_1 = 2;
+  int t_2 = 5;
+  
+  int temp;
   for(char i=0; i<3; i++){ 
       switch(i){
         case 0:
-        temp = 0;
+        temp = t_2;
         break;
         case 1:
         temp = t_1;
         break;
         case 2:
+        if(rtcTemp<0)
+        {
         temp = 11;
+        }
+        else   
+        {
+           temp = 10;
+        }
         break;
     }
        digitalWrite(Sseg_pins[i], HIGH);
@@ -98,10 +113,8 @@ void ShowTemperature()
        delay(3);
        digitalWrite(Sseg_pins[i], LOW);
     }
-    
+
   MH(Matrix_t);
-  counter--;
-  
 }
 
 void fill7seg( byte d ){
@@ -153,8 +166,7 @@ void Display7seg(int m1,int m2,int h2)
     }
 }
 
-
-
+//h1 -  отображает нужный символ на матрице
 void DisplayMatrix(int h1)
 {  
   byte Temp[3];
@@ -163,7 +175,7 @@ void DisplayMatrix(int h1)
         MH(Matrix_1);
         break;
         case 2:
-       MH(Matrix_2);
+        MH(Matrix_2);
         break;
         case 77:
         MH(Matrix_t);
@@ -176,42 +188,41 @@ void DisplayMatrix(int h1)
 
 void MH(byte Temp[3])
 {
-     for(char i=0; i<3; i++)
+  for(char i=0; i<3; i++)
     { 
        digitalWrite(Matrix_pins[i], HIGH);
        fillMatrix(i,Temp);
        delay(3);
        digitalWrite(Matrix_pins[i], LOW);
-    }  
-  }
+    } 
+}
 
-
-void Display()
+void Display(int t_min,int t_hour)
 { 
-  second = (second >> 4) * 10 + (second & 0x0F);
-  minute = (minute >> 4) * 10 + (minute & 0x0F);
-  hour   = (hour >> 4)   * 10 + (hour & 0x0F);
-
-  if(second%10 == 0)
-  {
-      counter = 17;
-  }
- 
-  
-  int m_1 = minute/10;
-  int m_2 = minute%10;
-  int h_1 = hour/10;
-  int h_2 = hour%10;
+  int m_1 = t_min/10;
+  int m_2 = t_min%10;
+  int h_1 = t_hour/10;
+  int h_2 = t_hour%10;
 
   Display7seg(m_1,m_2,h_2);
   DisplayMatrix(h_1);
-  counter--;
 }
 
 
 void setup() 
 {
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
+  myRTC.begin();    
+  setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  
   Wire.begin(); 
+  Timer1.initialize(1000000);         // initialize timer1, and set a 1/2 second period
+  Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
 
     for(int i=0; i<3; i++)
     {
@@ -230,28 +241,36 @@ void setup()
     pinMode(Mdata_pin, OUTPUT);
     pinMode(Msh_pin, OUTPUT);
     pinMode(Mst_pin, OUTPUT);  
-    
-    sensors.begin();            // начинаем работу с датчиком
+}
+
+void callback()
+{
+   sensors.requestTemperatures();    
 }
 
 void loop()
 {   
-    Wire.beginTransmission(0x68);                 // Start I2C protocol with DS3231 address
-    Wire.write(0);                                // Send register address
-    Wire.endTransmission(false);                  // I2C restart
-    Wire.requestFrom(0x68, 4);                    // Request 4 bytes from DS3231 and release I2C bus at end of reading
-    second = Wire.read();                         // Read seconds from register 0
-    minute = Wire.read();                         // Read minuts from register 1
-    hour   = Wire.read();    
+  time_t t = myRTC.get();    
+  int t_sec = second(t);
+  int t_min = minute(t);
+  int t_hour = hour(t);
 
-    // Read hour from register 2
-    if(counter > 10)
+  int count = 0;
+
+    if(t_sec%10 == 0)
     {
-      sensors.requestTemperatures();    
-      ShowTemperature();
+       sensors.requestTemperatures(); // Send the command to get temperatures
+       float temperature = sensors.getTempCByIndex(0);
+       count = 8;
+        Serial.print("Temperature for Device 1 is: ");
+        while(count != 0)
+        {
+          ShowTemperature(temperature);
+          count--;
+        } 
     }
-    else
-    {
-       Display();
-    } 
+      else
+      {
+       Display(t_min,t_hour);
+       } 
 }
